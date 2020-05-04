df6dd52cdb4fab2b6e218be09e3b3f4f3aa5e35e
Vaibhav Bhandari <vaibhavb@gmail.com>
2020-05-04 16:41:41 -0700

    Added support for actually editing sass and js files.

---
+++ assets/babeljs/main.js
@@ -0,0 +1,14 @@
+// The following code is based off a toggle menu by @Bradcomp
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  var burger = document.querySelector('.navbar-burger');
+  var menu = document.querySelector('#'+burger.dataset.target);
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+  });
+  $crisp.push(["safe", true])
+});
+
