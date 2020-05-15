de6e495d9122fe2e7e90fb1feb977abe27dc33ad
Vaibhav Bhandari <vaibhavb@gmail.com>
2020-05-15 13:08:54 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,6 @@
 
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-
-
 document.addEventListener('DOMContentLoaded', function () {
   var burger = document.querySelector('.navbar-burger');
   var menu = document.querySelector('#' + burger.dataset.target);
